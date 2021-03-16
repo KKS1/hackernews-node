ddc2a91617044ff3482aaae4c856d13a4e3f18cf
KanwaljeetSinghKalra <kkalra@roostify.com>
2021-03-16 12:05:03 -0700

    wip

--- src/utils.js
+++ src/utils.js
@@ -7,7 +7,7 @@ const getUserId = (req, authToken) => {
   if(req) {
     const authHeader = req.headers.authorization;
     if(authHeader){
-      const token = authHeader.replace('Bearer', '');
+      const token = authHeader.replace('Bearer ', '');
       if(!token) {
         throw new Error('No token found');
       }
